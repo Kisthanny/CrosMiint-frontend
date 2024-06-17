dd921ff8d12b562c97cc91f6aa31980a7939bb06
Kisthanny <tigerqige@gmail.com>
2024-06-18 07:49:36 +0800

    Fix: Subscribe drop input field add margin

--- app/components/SubscribeDrop/SubscribeDrop.tsx
+++ app/components/SubscribeDrop/SubscribeDrop.tsx
@@ -32,7 +32,7 @@ const SubscribeDrop = () => {
               Get premium magazines
             </p>
           </div>
-          <div className="flex items-center justify-between bg-gray-600 rounded-full px-4 py-2">
+          <div className="flex items-center justify-between bg-gray-600 rounded-full px-4 py-2 mt-2">
             <input
               value={userEmail}
               onChange={(e) => {
